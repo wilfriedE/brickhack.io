ec1f7961071e2ded420fecb102905454f8ff2f5c
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2016-12-29 19:57:39 -0500

    Example of schedule

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @event_schedule = Schedule.new("Event Schedule", "146w2Ozjkomtubbob03DWpUMN3vBOtCnxn_YCh82Vga0", "A1:E6", 0)
     render layout: "blank"
   end
 
